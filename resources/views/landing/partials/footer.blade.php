a602711b50dc6198c2a1582ecdac6b2f2ade08c1
paley777 <valleryan1212@gmail.com>
2022-09-04 20:55:24 +0700

    templating

---
+++ resources/views/landing/partials/footer.blade.php
@@ -0,0 +1,190 @@
+<style>
+    a {
+        -webkit-transition: .3s all ease;
+        -o-transition: .3s all ease;
+        transition: .3s all ease;
+    }
+
+    a,
+    a:hover {
+        text-decoration: none !important;
+    }
+
+    .content {
+        height: 70vh;
+    }
+
+    .footer-20192 {
+        position: relative;
+        color: #fff;
+        padding: 7rem 0;
+        background-image: linear-gradient(to top, #12145c 0%, #1d52a6 100%);
+    }
+
+    .footer-20192 .container {
+        position: relative;
+    }
+
+    .footer-20192 h3 {
+        font-size: 16px;
+        margin-bottom: 10px;
+        margin-top: 0;
+        line-height: 1.5;
+    }
+
+    .footer-20192 .links li {
+        margin-bottom: 10px;
+        line-height: 1.5;
+        display: block;
+    }
+
+    .footer-20192 .links li a {
+        color: #a6aabe;
+    }
+
+    .footer-20192 .links li a:hover {
+        color: #fff;
+    }
+
+    .footer-20192 .social li {
+        display: inline-block;
+        position: relative;
+    }
+
+    .footer-20192 .social li a {
+        position: relative;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        display: inline-block;
+        margin: 0;
+        padding: 0;
+        background-color: #8186d5;
+        color: #fff;
+    }
+
+    .footer-20192 .social li a>span {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+
+    .footer-20192 .footer-logo {
+        color: #fff;
+        font-size: 20px;
+        text-transform: uppercase;
+        letter-spacing: .1rem;
+    }
+
+    .footer-20192 .copyright {
+        color: #a6aabe;
+    }
+
+    .footer-20192 .cta {
+        -webkit-box-shadow: -20px -20px 0 0 rgba(54, 36, 134, 0.891);
+        box-shadow: -20px -20px 0 0 rgba(56, 62, 69, 0.2);
+        padding: 20px;
+        background-image: linear-gradient(to top, #fbff00 0%, #ffc400 100%);
+        top: -150px;
+        position: relative;
+    }
+
+    .footer-20192 .cta h2,
+    .footer-20192 .cta h3 {
+        line-height: 1.5;
+    }
+
+    .footer-20192 .cta h3 {
+        font-size: 20px;
+    }
+
+    .pattern {
+        background-image: url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='20' height='20' patternTransform='scale(2) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 100%, 0)'/><path d='M 10,-2.55e-7 V 20 Z M -1.1677362e-8,10 H 20 Z'  stroke-width='0.5' stroke='hsla(216, 0%, 97%, 0.13)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")
+    }
+</style>
+<style>
+    #slide {
+        position: relative;
+        left: -2000px;
+        -webkit-animation: slide 2s forwards;
+        -webkit-animation-delay: 1s;
+        animation: slide 2s forwards;
+        animation-delay: 1s;
+    }
+
+    @-webkit-keyframes slide {
+        100% {
+            left: 0;
+        }
+    }
+
+    @keyframes slide {
+        100% {
+            left: 0;
+        }
+    }
+</style>
+<footer class="footer-20192">
+    <div class="pattern">
+        <div class="site-section">
+            <div class="container">
+                <div class="cta d-block d-flex align-items-center px-5" id="slide">
+                    <div>
+                        <h2 class="mb-0 text-dark">Laman Pengaduan</h2>
+                        <h3 class="text-dark">Laporkan segala tindak berindikasi pelanggaran selingkung Universitas
+                            Bengkulu ke laman berikut.</h3>
+                    </div>
+                    <div class="ms-auto">
+                        <a href="#" class="btn btn-dark rounded-0 py-3 px-5">Lapor</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-4">
+                        <a href="#" class="footer-logo">Satuan Pengawas Internal</a>
+                        <p class="fs-5 footer-logo">Universitas Bengkulu</p>
+                        <p class="copyright">
+                            <small>&copy; 2022</small>
+                        </p>
+                    </div>
+                    <div class="col-3">
+                        <h3>Tautan Internal</h3>
+                        <ul class="list-unstyled links">
+                            <li><a href="https://www.unib.ac.id/">Universitas Bengkulu</a></li>
+                            <li><a href="https://pak.unib.ac.id/">Sistem Akademik</a></li>
+                            <li><a href="https://presensi.unib.ac.id/">Sistem Absensi Perkuliahan</a></li>
+                            <li><a href="http://siepel.unib.ac.id/">Sistem Evaluasi Pembelajaran</a></li>
+                            <li><a href="http://siepel.unib.ac.id/">Sistem E-Learning</a></li>
+                            <li><a href="http://ejournal.unib.ac.id/">Sistem E-Journal</a></li>
+                            <li><a href="http://repository.unib.ac.id/">Sistem Repository</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-3">
+                        <h3>Tautan Eksternal</h3>
+                        <ul class="list-unstyled links">
+                            <li><a href="https://www.kemdikbud.go.id/main/">Kementerian Pendidikan, Kebudayaan, Riset
+                                    dan Teknologi</a></li>
+                            <li><a href="http://blu.djpbn.kemenkeu.go.id/">Direktorat Pembinaan Pengelolaan Keuangan
+                                    BLU</a></li>
+                            <li><a href="https://bengkuluprov.go.id/">Pemerintah Provinsi Bengkulu</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-2">
+                        <h3>Kontak Kami</h3>
+                        <ul class="list-unstyled social">
+                            <li><a href="#"><span class="icon-facebook"></span></a></li>
+                            <li><a href="#"><span class="icon-twitter"></span></a></li>
+                            <li><a href="#"><span class="icon-linkedin"></span></a></li>
+                            <li><a href="#"><span class="icon-medium"></span></a></li>
+                            <li><a href="#"><span class="icon-paper-plane"></span></a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
